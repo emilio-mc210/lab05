437c46aa569ac79da55be6edf4a03d0ff96d8b35
emilio-mc210 <emilio.marques@ucr.ac.cr>
2025-06-01 15:49:58 -0600

    Programa inicial

---
+++ src/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "cJSON.h"
+
+int main(){
+    //Crear el objeto cJSON
+    cJSON *json = cJSON_CreateObject();
+    cJSON_AddStringToObject(json, "nombre", "Hector Bonifacio Echevarria Cervantes de la Cruz Arroyo Rojas");
+    cJSON_AddNumberToObject(json, "edad", 19);
+    cJSON_AddStringToObject(json, "carnet", "C12345");
+    cJSON_AddBoolToObject(json, "estudiante activo", cJSON_True);
+
+    //Convertir de objeto cJSON a JSON string
+    char *json_str = cJSON_Print(json);
+
+    //Escribir el JSON string a un documento
+    FILE *archivo = fopen("data.json", "w");
+    if(archivo == NULL){
+        printf("Error: No se pudo abrir el archivo.\n");
+        return 1;
+    }
+
+    printf("%s\n", json_str);
+    fputs(json_str, archivo);
+    fclose;
+
+    //Liberar memoria
+    cJSON_free(json_str);
+    cJSON_Delete(json);
+
+    return 0;
+}
+
